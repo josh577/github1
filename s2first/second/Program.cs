a62e6151337ffd925236af7596ce369ed3ee1c0c
Joshua Dodanduwa <10015281@student.toiohomai.ac.nz>
2018-07-25 09:55:01 +1200

    m

--- s2first/second/Program.cs
+++ s2first/second/Program.cs
@@ -10,10 +10,7 @@ namespace second
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i <= 4; i++)
-            {
-                Console.WriteLine("Hello World");
-            }
+            Console.WriteLine("Hello World");
             Console.ReadLine();
         }
     }
