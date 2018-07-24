c5c20d76fc9cc8cbd7b9c7db7c7133ea6d99df86
Joshua Dodanduwa <10015281@student.toiohomai.ac.nz>
2018-07-25 09:45:52 +1200

    first

---
+++ s2first/second/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace second
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 4; i++)
+            {
+                Console.WriteLine("Hello World");
+            }
+            Console.ReadLine();
+        }
+    }
+}
