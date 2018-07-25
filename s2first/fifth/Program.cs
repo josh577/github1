6636412045785425a14d67f662b56afd0f858c33
Joshua Dodanduwa <10015281@student.toiohomai.ac.nz>
2018-07-26 08:33:03 +1200

    Almost done

---
+++ s2first/fifth/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fifth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number between 1 and 12:");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i=1; i<=12; i++)
+            {
+                Console.WriteLine(num + "x" + i + "=" +(num*i));
+                
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ s2first/sixth/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sixth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second number:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Num1 is " +num2);
+            Console.WriteLine("Num2 is " +num1);
+            Console.ReadLine();
+        }
+    }
+}
