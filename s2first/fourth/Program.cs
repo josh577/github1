06702dabe1a15732232f511bfa9b118e39ef1a81
Diresh <bitbucket@ateamsoftware.com>
2018-07-25 17:36:28 +1200

    fourth

---
+++ s2first/fourth/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace fourth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey there stranger. To make me stop calling you stranger, enter in your first name:");
+            string fName = Console.ReadLine();
+
+            Console.WriteLine("Now enter in your last name:");
+            string lName = Console.ReadLine();
+
+            Console.WriteLine("Hey there " +fName + " " +lName);
+            Console.ReadLine();
+        }
+    }
+}
